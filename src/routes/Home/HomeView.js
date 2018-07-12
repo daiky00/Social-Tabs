77260b17c4dee06e6adcdd29b7f745f0d1ab3092
Francisco Pena <project.01@hotmail.com>
2018-07-11 23:24:09 -0500

    change some layout things and update a dependency

--- src/routes/Home/HomeView.js
+++ src/routes/Home/HomeView.js
@@ -29,7 +29,7 @@ const HomeView = () => (
           <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
             parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec
             pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
-          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis</p>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis Aenean massa. Cum sociis natoque penatibus et magnis dis</p>
         </Content>
         <Img src={me} />
       </Tab>
